2ebbb561ac5f3200c0f5bb18d412650b57d5ad99
Your Name <you@example.com>
2025-04-18 20:01:57 +0700

    TW4+React+Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+// import Header from "./Header.jsx";
+
+export default function App() {
+    return (
+      <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
+        <h1 className="font-bold text-indigo-400">✅ Tailwind is Working!</h1>
+        <p className="mt-4 text-md">This is styled with TailwindCSS.</p>
+      </div>
+    );
+  }
+  
